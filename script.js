456f409b5f66927c722a1e71bf5c26f8a6ba6ac8
shoguftaYousafzai <shoguftayousafzai12@gmail.com>
2025-06-18 13:58:33 -0500

    completed working on the script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+
+function changeBackground() {
+   const colors = ['#f0f8ff', '#ffe4e1', '#e6efa', '#fafad2', '#d1ffd6'];
+   const randamColor = colors[Math.floor(Math.random() *colors.length)];
+   document.body.style.backgroundcolor = randomcolor;
+
+}
